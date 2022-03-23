a7beebb42a548d047f67320ab63378069d48c71c
Administrator <kirillschwepz@gmail.com>
2022-03-23 13:59:57 +0700

    23 03 2022 1359 Готово.

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,18 @@
+
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 death;
+
+    @Autowired
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
